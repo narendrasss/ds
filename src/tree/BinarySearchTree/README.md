9b8df2f74b63914b9562a37caafe5be6b3f05159
Narendras <n.syahrasyad.n@gmail.com>
2019-06-17 09:38:06 -0700

    Started bst write up

--- src/tree/BinarySearchTree/README.md
+++ src/tree/BinarySearchTree/README.md
@@ -1,1 +1,17 @@
 # Binary Search Trees
+
+A binary search tree or BST for short is a particular type of tree that keeps its nodes in sorted order.
+
+In a BST, each node in a node's **left** subtree is comparatively smaller than itself, while each node in its **right** subtree is comparatively larger.
+
+Below is a tree of height 4 with 8 at its root (also the output if you call `tree.print()`!):
+
+```
+        8      
+    ┌───┴───┐  
+    3       10  
+  ┌─┴─┐     └─┐
+  1   6       14
+     ┌┴┐     ┌┘
+     4 7     13 
+```
--- src/tree/BinarySearchTree/bst.test.ts
+++ src/tree/BinarySearchTree/bst.test.ts
@@ -4,15 +4,11 @@ describe('bst', () => {
   const comparator = jest.fn((a: number, b: number) => b - a)
 
   test('integration', () => {
-    const values = [8, 2, 19, 7, 18, 6, 10, 3, 13, 11, 17, 16]
+    const values = [8, 3, 10, 1, 6, 14, 4, 7, 13]
     const tree = new BinarySearchTree(comparator, ...values)
 
     console.log(tree.print())
     expect(tree.size).toBe(values.length)
-
-    tree.remove(19).remove(13)
-    console.log(tree.print())
-    expect(tree.size).toBe(values.length - 2)
 
     const node = tree.find(10)
     expect(node.value).toEqual(10)
